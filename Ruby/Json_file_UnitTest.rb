e711297b840d77e35a6dd94ec5a8cf044614758e
AnisShaikh10 <anisofficial0208@gmail.com>
2023-01-23 10:59:17 +0530

    Added Ruby Assignment

---
+++ Ruby/Json_file_UnitTest.rb
@@ -0,0 +1,20 @@
+require 'test/unit'
+
+require_relative 'Json_file'
+
+class Test_JsonFile < Test::Unit::TestCase
+  def test_process_json_file
+    filename_location = "C:\\Users\\AS\\Desktop\\Infuse\\Workspace\\Ruby\\Read_file.json"
+    assert_equal({"name"=>"Nicole Smith", "age"=>25, "salary"=>25552.67, "firstname"=>"Nicole", "lastname"=>"Smith"}, process_json_file(filename_location))
+  end
+
+  def test_process_json_file_with_invalid_location
+    filename_location = "C:\\Users\\AS\\Desktop\\Infuse\\Workspace\\Read_file.json"
+    assert_equal("Invalid File Path or No such File or Directory", process_json_file(filename_location))
+  end
+
+  def test_process_json_file_with_invalid_file
+    filename_location = "C:\\Users\\AS\\Desktop\\Infuse\\Workspace\\Content_file.txt"
+    assert_equal("Invalid File Path or No such File or Directory", process_json_file(filename_location))
+  end
+end
---
+++ Ruby/iteration.rb
@@ -0,0 +1,9 @@
+array = [2 ,3, 5, 6, 7]
+
+def iterate_array(arr)
+    arr.each do |i|
+        puts i
+    end
+end
+
+iterate_array(array)
