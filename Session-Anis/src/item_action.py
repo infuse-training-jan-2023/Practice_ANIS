92a330a042dd78131726d5ce059bb78056d8dce0
AnisShaikh10 <anisofficial0208@gmail.com>
2023-02-03 15:30:25 +0530

    added todo api Q4

--- Session-Anis/src/item_action.py
+++ Session-Anis/src/item_action.py
@@ -1,31 +1,94 @@
 from item_repository import ItemRepositiory
 
+
 class ItemActions:
-    def __init__(self) -> None:
-        self.item_repo = ItemRepositiory()
+	def __init__(self) -> None:
+		self.item_repo = ItemRepositiory()
 
-    def get_all_items(self):
-        try:
-            items = self.item_repo.get_all_items()
-            res = []
-            for item in items:
-                res.append(
-                    {
-                        'id': item[0],
-                        'item': item[1],
-                        'status': item[2],
-                        'reminder': item[3]
-                    }
-                )
-            return res
-        except Exception as e:
-            print(e)
-            return {}
+	def get_all_items(self):
+		try:
+			items = self.item_repo.get_all_items()
+			res = []
+			for item in items:
+				res.append(
+					{
+						'id': item[0],
+						'item': item[1],
+						'status': item[2],
+						'reminder': item[3]
+					}
+				)
+			return res
+		except Exception as e:
+			print(e)
+			return {}
 
-    def add_item(self, item, reminder):
-        try:
-            item = self.item_repo.add_items(item, reminder)
-            return item
-        except Exception as e:
-            print(e)
-            return {}
+	def get_item(self, item):
+		try:
+			items = self.item_repo.get_item(item)
+			res = []
+			for item in items:
+				res.append(
+					{
+						'id': item[0],
+						'item': item[1],
+						'status': item[2],
+						'reminder': item[3]
+					}
+				)
+			return res
+		except Exception as e:
+			print(e)
+			return {}
+
+	def add_item(self, item, reminder):
+		try:
+			item = self.item_repo.add_item(item, reminder)
+			return item
+		except Exception as e:
+			print(e)
+			return {}
+
+	def delete_item(self, id):
+		try:
+			items = self.item_repo.delete_item(id)
+			res = ["Item Deleted Successfully"]
+			return res
+		except Exception as e:
+			print(e)
+			return {}
+
+	def update_item(self, data, id):
+		try:
+			items = self.item_repo.update_item(data, id)
+			res = ["Item Updated Successfully"]
+			return res
+		except Exception as e:
+			print(e)
+			return {}
+
+	def add_user(self, name, address, mob):
+		try:
+			item = self.item_repo.add_user(name, address, mob)
+			return item
+		except Exception as e:
+			print(e)
+			return {}
+
+	def save_data_to_file(self):
+		try:
+			items = self.item_repo.save_data_to_file()
+			res = []
+			for item in items:
+				res.append(
+					{
+						'id': item[0],
+						'item': item[1],
+						'status': item[2],
+						'reminder': item[3]
+					}
+				)
+			return res
+		except Exception as e:
+			print(e)
+			return {}
--- Session-Anis/src/item_repository.py
+++ Session-Anis/src/item_repository.py
@@ -1,38 +1,125 @@
 import sqlite3
+import csv
 
 class ItemRepositiory:
-    NOT_STARTED = "Not Started"
-    DBPATH = './todo.db'
+	NOT_STARTED = "Not Started"
+	DBPATH = './todo.db'
 
-    @staticmethod
-    def connect_db():
-        return sqlite3.connect(ItemRepositiory.DBPATH)
+	@staticmethod
+	def connect_db():
+		return sqlite3.connect(ItemRepositiory.DBPATH)
 
-    @staticmethod
-    def get_all_items():
-        try:
-            conn = ItemRepositiory.connect_db()
-            c = conn.cursor()
-            rows = c.execute('select * from items')
-            print("Rows Value: " ,rows)
-            return rows
-        except Exception as e:
-            raise Exception('Error: ',e)
+	@staticmethod
+	def get_all_items():
+		try:
+			conn = ItemRepositiory.connect_db()
+			c = conn.cursor()
+			rows = c.execute('select * from items')
+			print("Rows Value: " ,rows)
+			return rows
+		except Exception as e:
+			raise Exception('Error: ',e)
 
-    @staticmethod
-    def add_items(item, reminder):
-        try:
-            conn = ItemRepositiory.connect_db()
-            c = conn.cursor()
-            insert_cursor = c.execute('insert into items (item, status,reminder) VALUES (?,?,?)',
-            (item, ItemRepositiory.NOT_STARTED, reminder))
-            conn.commit()
-            return
-            {
-                'id': insert_cursor.lastrowid,
-                'item': item,
-                'status': ItemRepositiory.NOT_STARTED,
-                'reminder': reminder
-            }
-        except Exception as e:
-            raise Exception('Error: ', e)
+	@staticmethod
+	def get_item(item):
+		try:
+			conn = ItemRepositiory.connect_db()
+			c = conn.cursor()
+			rows = c.execute('select * from items where id=?',(item,))
+			print("Rows Value: " ,rows)
+			return rows
+		except Exception as e:
+			raise Exception('Error: ',e)
+
+	@staticmethod
+	def add_item(item, reminder):
+		try:
+			conn = ItemRepositiory.connect_db()
+			c = conn.cursor()
+			insert_cursor = c.execute('insert into items (item, status,reminder) VALUES (?,?,?)',
+			(item, ItemRepositiory.NOT_STARTED, reminder))
+			conn.commit()
+			return{
+				'id': insert_cursor.lastrowid,
+				'item': item,
+				'status': ItemRepositiory.NOT_STARTED,
+				'reminder': reminder
+			}
+		except Exception as e:
+			raise Exception('Error: ', e)
+
+	@staticmethod
+	def delete_item(id):
+		try:
+			conn = ItemRepositiory.connect_db()
+			c = conn.cursor()
+			fetch_status = c.execute('select item from items where id=?', (id, ))
+			if fetch_status.fetchone()[0] == 0:
+				raise TypeError("Item Dosen't Exist")
+			rows = c.execute('DELETE FROM items WHERE id=?',(id,))
+			conn.commit()
+			print("Rows value:",rows)
+			return rows
+		except Exception as e:
+			raise Exception('Error: ', e)
+
+	@staticmethod
+	def update_item(data, id):
+		print(id)
+		print(data)
+		try:
+			conn = ItemRepositiory.connect_db()
+			c = conn.cursor()
+			fetch_status = c.execute('select id from items where id=?', (id, ))
+			if fetch_status.fetchone()[0] == '':
+				raise TypeError("Item Dosen't Exist")
+			for key, value in data.items():
+				rows = c.execute(f'UPDATE items SET {key}=? WHERE id=?', (value, id ))
+			conn.commit()
+			print("Rows value:",rows)
+			return rows
+		except Exception as e:
+			raise Exception('Error: ', e)
+
+	@staticmethod
+	def add_user(name, address, mob):
+		try:
+			conn = ItemRepositiory.connect_db()
+			c = conn.cursor()
+			insert_cursor = c.execute('insert into Users (name, address, mobile) VALUES (?,?,?)',
+			(name, address, mob))
+			conn.commit()
+			return{
+				'id': insert_cursor.lastrowid,
+				'name': name,
+				'address': address,
+				'mobile': mob
+			}
+		except Exception as e:
+			raise Exception('Error: ', e)
+
+	@staticmethod
+	def save_data_to_file():
+		try:
+			conn = ItemRepositiory.connect_db()
+			c = conn.cursor()
+			rows = c.execute('select * from items')
+			print("Rows Value: " ,rows)
+			fields = ['ID', 'ITEM', 'STATUS', 'REMINDER']
+			# writing to csv file
+			with open('ItemsRecords.csv', 'w') as csvfile:
+				# creating a csv writer object
+				csvwriter = csv.writer(csvfile)
+				# writing the fields
+				csvwriter.writerow(fields)
+
+				# writing the data rows
+				csvwriter.writerows(rows)
+
+			return rows
+		except Exception as e:
+			raise Exception('Error: ',e)
+
+
+
+
